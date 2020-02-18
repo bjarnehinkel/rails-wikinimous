7705ed56696370eaca80d8cae0f123c535ec81c5
Bjarne Hinkel <bjarne@hinkels.de>
2020-02-18 17:01:52 +0100

    the wiki works with all aspects of CRUD and looks nice with bootstrap style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,22 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
